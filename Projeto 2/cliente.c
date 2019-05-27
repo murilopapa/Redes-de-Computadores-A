c98c618f8dc95b56bd1629617e6bc3822005cb23
murilopapa <murilopapa123@gmail.com>
2019-05-27 20:57:05 -0300

    opcao de ler mensagens

--- Projeto 2/cliente.c
+++ Projeto 2/cliente.c
@@ -30,10 +30,20 @@ struct grupo
     struct grupo *prox;
 };
 
+struct mensagem
+{
+    char mensagem[200];
+    char telefone[9];
+    int leitura;
+    struct mensagem *prox;
+};
+
 //variaveis globais
 pthread_t thread_id;
 int porta;
-struct contato *raiz = NULL;
+struct contato *raiz_contato = NULL;
+struct grupo *raiz_grupo = NULL;
+struct mensagem *raiz_mensagem = NULL;
 
 //prototipos
 void *servidor();
@@ -140,6 +150,57 @@ char **argv;
         exit(5);
     } //informa ao servidor o numero de telefone
 
+    char nome_arquivo_contatos[50];
+    strcpy(nome_arquivo_contatos, "contatos_");
+    strcat(nome_arquivo_contatos, telefone);
+    strcat(nome_arquivo_contatos, ".dat");
+
+    arquivoContatos = fopen(nome_arquivo_contatos, "ab");
+    if (!arquivoContatos)
+    {
+        printf("\nErro na abertura do arquivo.\n");
+    }
+    else
+    {
+        fclose(arquivoContatos);
+    }
+
+    arquivoContatos = fopen(nome_arquivo_contatos, "rb");
+    if (!arquivoContatos)
+    {
+        printf("\nErro na abertura do arquivo.\n");
+    }
+    else
+    {
+        printf("\n");
+        int idTemporario = 1;
+        while (fread(&lerNome, sizeof(lerNome), 1, arquivoContatos) != NULL)
+        {
+            struct contato *novo = (struct contato *)malloc(sizeof(struct contato));
+            struct contato *aux;
+            fread(&lerTelefone, sizeof(lerTelefone), 1, arquivoContatos);
+            strcpy(novo->nome, lerNome);
+            strcpy(novo->telefone, lerTelefone);
+            novo->prox = NULL;
+            printf("[%d] %s\t\t%s\n", idTemporario, novo->nome, novo->telefone);
+            idTemporario++;
+            if (raiz_contato == NULL)
+            {
+                raiz_contato = novo;
+            }
+            else
+            {
+                aux = raiz_contato;
+                while (aux->prox != NULL)
+                {
+                    aux = aux->prox;
+                }
+                aux->prox = novo;
+            }
+        }
+        fclose(arquivoContatos);
+    }
+
     int op;
     do
     {
@@ -154,7 +215,8 @@ char **argv;
         printf("\n2) Grupos");
         printf("\n3) Cadastrar usuário");
         printf("\n4) Criar grupo");
-        printf("\n5) Sair\n");
+        printf("\n5) Ler mensagens");
+        printf("\n6) Sair\n");
         printf("\nOpção: ");
 
         fflush(stdin);
@@ -169,129 +231,133 @@ char **argv;
         case 1:
             // enviar mensagem
 
-            arquivoContatos = fopen("contatos.dat", "rb");
-            if (!arquivoContatos)
-            {
-                printf("\nErro na abertura do arquivo.\n");
-            }
-            else
-            {
-                printf("\n");
-                int idTemporario = 1;
-                while (fread(&lerNome, sizeof(lerNome), 1, arquivoContatos) != NULL)
-                {
-                    struct contato *novo = (struct contato *)malloc(sizeof(struct contato));
-                    struct contato *aux;
-                    fread(&lerTelefone, sizeof(lerTelefone), 1, arquivoContatos);
-                    strcpy(novo->nome, lerNome);
-                    strcpy(novo->telefone, lerTelefone);
-                    novo->prox = NULL;
-                    printf("[%d] %s\t\t%s\n", idTemporario, novo->nome, novo->telefone);
-                    idTemporario++;
-                    if (raiz == NULL)
-                    {
-                        raiz = novo;
-                    }
-                    else
-                    {
-                        aux = raiz;
-                        while (aux->prox != NULL)
-                        {
-                            aux = aux->prox;
-                        }
-                        aux->prox = novo;
-                    }
-                }
-                fclose(arquivoContatos);
-            }
-
             //aqui o cliente esta apenas verificando se o numero
-            printf("\nDeseja enviar msg para qual numero: ");
-            fflush(stdin);
-            gets(num_pesquisar);
-            fflush(stdin);
-
-            if (send(s, num_pesquisar, sizeof(num_pesquisar), 0) < 0)
+            printf("\nDeseja enviar msg para qual numero?\n");
+            struct contato *aux1;
+            int count = 0;
+            aux1 = raiz_contato;
+            if (aux1 == NULL)
             {
-                perror("Send()");
-                exit(5);
-            }
-            int retorno = recv(s, recvbuf, sizeof(recvbuf), 0); //recebe a mensagem do cliente e verifica o valor de retorno
-
-            if (retorno == -1)
-            {
-                perror("Recvbuf()");
-                close(s);
-                exit(1);
-            }
-            else if (retorno == 0)
-            {
-                printf("thread encerrada pois o cliente foi fechado num momento inesperado\n");
-                close(s);
-                exit(1);
-            }
-            if (strcmp("offline", recvbuf) == 0)
-            {
-                printf("\nCliente offline\n");
+                printf("\nSem contatos salvos!\n");
             }
             else
             {
-                strcpy(ipAtual, strtok(recvbuf, "+"));
-                strcpy(portaAtual, strtok(NULL, "$"));
-
-                printf("Ip atual: %s\n", recvbuf);
-                printf("Porta atual: %s", portaAtual);
-
-                unsigned short port2;
-                char sendbuf2[101];
-                struct hostent *hostnm2;
-                struct sockaddr_in server2;
-                int s2;
-
-                hostnm2 = gethostbyname(ipAtual);
-                if (hostnm2 == (struct hostent *)0)
+                while (aux1->prox != NULL)
                 {
-                    fprintf(stderr, "Gethostbyname failed\n");
-                    exit(2);
+                    printf("%d) NOME: %s - TELEFONE: %s\n", count, aux1->nome, aux1->telefone);
+                    aux1 = aux1->prox;
+                    count++;
                 }
-                port2 = (unsigned short)atoi(portaAtual);
+                printf("%d) NOME: %s - TELEFONE: %s\n", count, aux1->nome, aux1->telefone);
 
-                /*
-                * Define o endere�o IP e a porta do servidor
-                */
-                server2.sin_family = AF_INET;
-                server2.sin_port = htons(port2);
-                server2.sin_addr.s_addr = *((unsigned long *)hostnm2->h_addr);
+                char opcao[10];
+                int opcao_int = 0;
 
-                /*
-                * Cria um socket TCP (stream)
-                */
-                if ((s2 = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+                do
                 {
-                    perror("Socket()");
-                    exit(3);
+                    printf("\nOpcao: ");
+                    fflush(stdin);
+                    gets(opcao);
+                    fflush(stdin);
+
+                    opcao_int = atoi(opcao);
+                } while (opcao_int > count);
+
+                aux1 = raiz_contato;
+
+                for (int i = 0; i < opcao_int; i++)
+                {
+                    aux1 = aux1->prox;
                 }
 
-                /* Estabelece conex�o com o servidor */
-                if (connect(s2, (struct sockaddr *)&server2, sizeof(server2)) < 0)
-                {
-                    perror("Connect()");
-                    exit(4);
-                }
-                char mensagem[100];
-                printf("\nMensagem: ");
+                strcpy(num_pesquisar, aux1->telefone);
+                printf("Mandando msg para: %s\n", num_pesquisar);
 
-                fflush(stdin);
-                gets(mensagem);
-                fflush(stdin);
-
-                printf("Msg enviada: %s", mensagem);
-                if (send(s2, mensagem, strlen(mensagem) + 1, 0) < 0)
+                if (send(s, num_pesquisar, sizeof(num_pesquisar), 0) < 0)
                 {
                     perror("Send()");
                     exit(5);
-                } //informa ao servidor o numero de telefone
-                close(s2);
+                }
+                int retorno = recv(s, recvbuf, sizeof(recvbuf), 0); //recebe a mensagem do cliente e verifica o valor de retorno
+
+                if (retorno == -1)
+                {
+                    perror("Recvbuf()");
+                    close(s);
+                    exit(1);
+                }
+                else if (retorno == 0)
+                {
+                    printf("thread encerrada pois o cliente foi fechado num momento inesperado\n");
+                    close(s);
+                    exit(1);
+                }
+                if (strcmp("offline", recvbuf) == 0)
+                {
+                    printf("\nCliente offline\n");
+                }
+                else
+                {
+                    strcpy(ipAtual, strtok(recvbuf, "+"));
+                    strcpy(portaAtual, strtok(NULL, "$"));
+
+                    //printf("Ip atual: %s\n", recvbuf);
+                    //printf("Porta atual: %s", portaAtual);
+
+                    unsigned short port2;
+                    char sendbuf2[101];
+                    struct hostent *hostnm2;
+                    struct sockaddr_in server2;
+                    int s2;
+
+                    hostnm2 = gethostbyname(ipAtual);
+                    if (hostnm2 == (struct hostent *)0)
+                    {
+                        fprintf(stderr, "Gethostbyname failed\n");
+                        exit(2);
+                    }
+                    port2 = (unsigned short)atoi(portaAtual);
+
+                    /*
+                * Define o endere�o IP e a porta do servidor
+                */
+                    server2.sin_family = AF_INET;
+                    server2.sin_port = htons(port2);
+                    server2.sin_addr.s_addr = *((unsigned long *)hostnm2->h_addr);
+
+                    /*
+                * Cria um socket TCP (stream)
+                */
+                    if ((s2 = socket(PF_INET, SOCK_STREAM, 0)) < 0)
+                    {
+                        perror("Socket()");
+                        exit(3);
+                    }
+
+                    /* Estabelece conex�o com o servidor */
+                    if (connect(s2, (struct sockaddr *)&server2, sizeof(server2)) < 0)
+                    {
+                        perror("Connect()");
+                        exit(4);
+                    }
+                    char mensagem[100];
+                    printf("\nMensagem: ");
+
+                    fflush(stdin);
+                    gets(mensagem);
+                    fflush(stdin);
+
+                    //printf("Msg enviada: %s", mensagem);
+                    strcat(mensagem, "&");
+                    strcat(mensagem, telefone);
+                    strcat(mensagem, "$");
+                    if (send(s2, mensagem, strlen(mensagem) + 1, 0) < 0)
+                    {
+                        perror("Send()");
+                        exit(5);
+                    } //informa ao servidor o numero de telefone
+                    close(s2);
+                }
             }
             break;
 
@@ -301,11 +367,36 @@ char **argv;
             break;
 
         case 3: // Registrar usuário
+
             printf("Nome do contato: ");
-            scanf("%s", &salvarNome);
+            fflush(stdin);
+            gets(salvarNome);
+            fflush(stdin);
             printf("Telefone: ");
-            scanf("%s", &salvarTelefone);
-            arquivoContatos = fopen("contatos.dat", "ab");
+            fflush(stdin);
+            gets(salvarTelefone);
+            fflush(stdin);
+
+            struct contato *aux_contato;
+            struct contato *novo_contato = (struct contato *)malloc(sizeof(struct contato));
+
+            strcpy(novo_contato->nome, salvarNome);
+            novo_contato->prox = NULL;
+            strcpy(novo_contato->telefone, salvarTelefone);
+            if (raiz_contato == NULL)
+            {
+                raiz_contato = novo_contato;
+            }
+            else
+            {
+                aux_contato = raiz_contato;
+                while (aux_contato->prox)
+                {
+                    aux_contato = aux_contato->prox;
+                }
+                aux_contato->prox = novo_contato;
+            }
+            arquivoContatos = fopen(nome_arquivo_contatos, "ab");
             if (!arquivoContatos)
             {
                 printf("\nErro na abertura do arquivo.\n");
@@ -320,14 +411,48 @@ char **argv;
             break;
         case 4: // Cria grupo
             break;
-        case 5: // Sair
+        case 5: // Ler msg
+            printf("\n TODAS AS MENSAGENS\n");
+            struct mensagem *aux = raiz_mensagem;
+            if (aux == NULL)
+            {
+                printf("NENHUMA MENSAGEM\n");
+            }
+            else
+            {
+                while (aux->prox != NULL)
+                {
+                    if (aux->leitura == 0)
+                    {
+                        printf("*NOVA*[%s] - %s\n", aux->telefone, aux->mensagem);
+                        aux->leitura = 1;
+                        aux = aux->prox;
+                    }
+                    else
+                    {
+                        printf("[%s] - %s\n", aux->telefone, aux->mensagem);
+                        aux = aux->prox;
+                    }
+                }
+                if (aux->leitura == 0)
+                {
+                    printf("*NOVA*[%s] - %s\n", aux->telefone, aux->mensagem);
+                    aux->leitura = 1;
+                }
+                else
+                {
+                    printf("[%s] - %s\n", aux->telefone, aux->mensagem);
+                }
+            }
+            break;
+        case 6: // Sair
             break;
         default:
             printf("Opcao Inválida!\n");
             break;
         }
 
-    } while (op != 5);
+    } while (op != 6);
 
     /* Fecha o socket */
     close(s);
@@ -369,6 +494,8 @@ void *servidor()
     }
     while (1)
     {
+        struct mensagem *novo = (struct mensagem *)malloc(sizeof(struct mensagem));
+        struct mensagem *aux;
         int namelen = sizeof(client);
         if ((ns = accept(s, (struct sockaddr *)&client, &namelen)) == -1)
         {
@@ -390,7 +517,25 @@ void *servidor()
             pthread_exit(NULL);
         }
         //printa a msg
-        printf("RECEBIDO: %s", recvbuf);
+
+        strcpy(novo->mensagem, strtok(recvbuf, "&"));
+        strcpy(novo->telefone, strtok(NULL, "$"));
+        novo->leitura = 0;
+        novo->prox = NULL;
+
+        if (raiz_mensagem == NULL)
+        {
+            raiz_mensagem = novo;
+        }
+        else
+        {
+            aux = raiz_mensagem;
+            while (aux->prox != NULL)
+            {
+                aux = aux->prox;
+            }
+            aux->prox = novo;
+        }
         //fecha o socket
         close(ns);
     }
